1d3fddef6c1b2946814ab7b8b8c67a4ea2cb1b82
Naama Kari <naama@gmail.com>
2022-05-08 00:25:13 +0300

    One line is better

--- week6.4-ex1.py
+++ week6.4-ex1.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 
-def find_the_massage(image_path: str) -> str:
+def find_the_message(image_path: str) -> str:
     """
     The function gets a path to the image and checks where the black pixels are in it
      and decodes the message encrypted there by the row number of the black pixels.
@@ -10,11 +10,10 @@ def find_the_massage(image_path: str) -> str:
     """
     image = Image.open(image_path, 'r')
     width, height = image.size
-    # taking just the pixel of the row
-    black_pixel = [j for i in range(width) for j in range(height) if image.getpixel((i, j)) == 1]
-    return ''.join([chr(pixel) for pixel in black_pixel])
+    # Taking just the pixel of the row.
+    return ''.join(map(str, [chr(j) for i in range(width) for j in range(height) if image.getpixel((i, j)) == 1]))
 
 
 if __name__ == '__main__':
     path_image = input("Please insert the path of the image:\n")
-    print(f"The encrypted message is:\n{find_the_massage(path_image)}")
+    print(f"The encrypted message is:\n{find_the_message(path_image)}")
